c011b6a8f7d422a8448c895005a394143b27957e
Kotaimen.Cao <kotaimen.c@gmail.com>
2013-01-25 16:23:26 +0800

    Update previews and to mason config 0.9.3

--- README.md
+++ README.md
@@ -5,11 +5,16 @@ High Quality Terrain Shaded Relief (with "Illuminated" effect).
 
 Sample render uses OSM Waterbody, Natural Earth II land cover, SRTM30, NED 1/3 arc second, NED 1/9 arc second.
 
+Requires to mason 0.9.3.
+
 Olympic National Park:
 ![Sample 1](https://github.com/Kotaimen/maps-Terrain/raw/master/sample-0.jpg)
 
 Mt. Rainer:
 ![Sample 2](https://github.com/Kotaimen/maps-Terrain/raw/master/sample-1.jpg)
+
+San Francisco:
+![Sample 2](https://github.com/Kotaimen/maps-Terrain/raw/master/sample-2.jpg)
 
 Salt Lake City:
 ![Sample 3](https://github.com/Kotaimen/maps-Terrain/raw/master/sample-3.jpg)
--- relief.cfg.py
+++ relief.cfg.py
@@ -5,47 +5,55 @@ zfactor = 4 #14 8 6 4
 azimuth = 325 
 datadir = '/Users/Kotaimen/proj/geodata'
 themedir= './themes/Terrain'
-cachedir= os.path.join(themedir, 'cache')
+cachedir_export= os.path.join(themedir, 'cache')
 tag = 'Terrain'
 tile_size = 256
 fmt = 'jpg'
 
+cachedir=os.path.join('/tmp', tag, 'cache')
+
 elev_1km = dict(\
     prototype='datasource.dataset',
     dataset_path=os.path.join(datadir, 'srtm30_new/world_tiled.tif'),
+    keep_cache=False,    
     cache=dict(prototype='metacache',
         root=os.path.join(cachedir, 'elevation'),
-        compress=True,
+        compress=False,
         data_format='gtiff',
         ),
     )
-
-elev_30m = dict(\
+   
+elev_100m = dict(\
     prototype='datasource.dataset',
-    dataset_path=os.path.join(datadir, 'SRTM_30_org/world/fill/world.vrt'),
+    dataset_path=os.path.join(datadir, 'DEM-Tools-patch/source/ned100m/world_3857.tif'),
+    keep_cache=False,    
     cache=dict(prototype='metacache',
         root=os.path.join(cachedir, 'elevation'),
-        compress=True,
+        compress=False,
         data_format='gtiff',
         ),
     )
     
-elev_100m = dict(\
+elev_30m = dict(\
     prototype='datasource.dataset',
-    dataset_path=os.path.join(datadir, 'DEM-Tools-patch/source/ned100m/world_3857.tif'),
+#    dataset_path=os.path.join(datadir, '/home/pset/proj/geodata/SRTM_30_org/world/fill/world_3857.vrt'),
+    dataset_path=os.path.join(datadir, '/data/tilestorage/geodata/world_srtm30.tif'),
+    keep_cache=False,    
     cache=dict(prototype='metacache',
         root=os.path.join(cachedir, 'elevation'),
-        compress=True,
+        compress=False,
         data_format='gtiff',
         ),
+    resample_method='bilinear'
     )
 
 elev_10m = dict(\
     prototype='datasource.dataset',
-    dataset_path=os.path.join(datadir, 'DEM-Tools-patch/source/ned10m/world_3857.tif'),    
+    dataset_path=os.path.join(datadir, '/data/tilestorage/geodata/world_3857.tif'),    
+    keep_cache=False,    
     cache=dict(prototype='metacache',
         root=os.path.join(cachedir, 'elevation'),
-        compress=True,
+        compress=False,
         data_format='gtiff',
         ),
     )
@@ -53,19 +61,19 @@ elev_10m = dict(\
 elev_3m = dict(\
     prototype='datasource.dataset',
     dataset_path=os.path.join(datadir, 'st-helens/st-helens.vrt'),
+    keep_cache=False,    
     cache=dict(prototype='metacache',
         root=os.path.join(cachedir, 'elevation'),
-        compress=True,
+        compress=False,
         data_format='gtiff',
         ),
     )
 
 elevation = dict(\
     prototype='composite.selector',
-    sources = ['elev_1km', 'elev_100m', 'elev_10m', 'elev_3m'],
-    
+    sources = ['elev_1km', 'elev_100m', 'elev_10m', 'elev_3m'],   
     #            0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18
-    condition = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
+    condition = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3],
     )
 
 diffuse = dict(\
@@ -125,15 +133,10 @@ roads_labels = dict(\
 
 composer = dict(\
     prototype='composite.imagemagick',
-    sources=['diffuse', 'detail', 'specular', 'landcover', 'waterbody', 'roads_labels'],
-    cache=dict(prototype='metacache',
-              root=os.path.join(cachedir, '%s' % tag),
-              data_format=fmt,
-             ),    
+    sources=['diffuse', 'detail', 'specular', 'landcover', 'waterbody', 'roads_labels'], 
     format='jpg',
     command='''   
     (
-#          
          ( $1 -fill grey50 -colorize 100% )
          ( $1 ) -compose blend -define compose:args=30% -composite
          ( $2 -fill #003cff -tint 65 -gamma 0.8  ) -compose blend -define compose:args=40% -composite
@@ -145,7 +148,7 @@ composer = dict(\
          -sigmoidal-contrast 2.5
          -sharpen 0x0.75
          ( $6 ) -compose Over -composite
-         -quality 95
+         -quality 89
     )
     '''
     )
@@ -155,16 +158,17 @@ ROOT = dict(\
     renderer='composer',
     metadata=dict(tag=tag,
                   version='1.0',
-                  description='Shaded Relief Map of Mt St.Halen',
-                  attribution='Open Street Map, SRTM Plus, SRTM30, NED 1/3 Arc Second, NED 1/9 Arc Second',
+                  description='Shaded Relief Map',
+                  attribution='Open Street Map, SRTM Plus, SRTM30, NED 1/3", NED 1/9"',
                   ),
-    cache=dict(prototype='filesystem',
-              root=os.path.join(cachedir, 'export', '%s' % tag),
-              data_format=fmt,
-              simple=True
-             ),
-    pyramid=dict(levels=range(4, 15),
-#                  envelope=[-124,34,-70,48],            
+    cache=dict(prototype='cluster',
+               stride=16,
+               servers=['localhost:11211',],
+               root=os.path.join(cachedir_export, 'export', '%s' % tag),
+              ),
+
+    pyramid=dict(levels=range(4, 16),
+                 envelope=[-124,34,-70,48],            
 #                  envelope=[-125,34,-105,48.5],        
                  zoom=7,
                  center=(-122.1897, 46.2024),
